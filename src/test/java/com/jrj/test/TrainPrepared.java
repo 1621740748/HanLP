f722e8665debfed90ae7b91f8dc5481d0607d356
huangyan <1621740748@qq.com>
2019-11-05 17:39:52 +0800

    测试停用词

--- src/test/java/com/jrj/test/TrainPrepared.java
+++ src/test/java/com/jrj/test/TrainPrepared.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import com.alibaba.fastjson.JSON;
 import com.hankcs.hanlp.HanLP;
+import com.hankcs.hanlp.dictionary.stopword.CoreStopWordDictionary;
 import com.hankcs.hanlp.seg.common.Term;
 
 public class TrainPrepared {
@@ -20,23 +21,26 @@ public class TrainPrepared {
 		try {
 			fi=new FileInputStream(f);
 			InputStreamReader fileReader = new InputStreamReader(fi,"utf-8");
-			 fp = new BufferedReader(fileReader);
+			fp = new BufferedReader(fileReader);
 			String line;
 			int i=0;
 			do {
 				line=fp.readLine();
 				String l=JSON.parseObject(line).getString("content");
-	            List<Term> termList = HanLP.segment(l);
-	            for(Term t:termList) {
-	            	  System.out.print(t.word+" ");
-	            }
-	            System.out.println();
+				List<Term> termList = HanLP.segment(l);
+				for(Term t:termList) {
+					if(CoreStopWordDictionary.contains(t.word)) {
+						continue;
+					}
+					System.out.print(t.word+" ");
+				}
+				System.out.println();
 				i++;
 				if(i>100)break;
 			}while(line!=null);
-			
+
 		} catch (FileNotFoundException e) {
-		
+
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -47,7 +51,7 @@ public class TrainPrepared {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
+
 		}
 
 	}
