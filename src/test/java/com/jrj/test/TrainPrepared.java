c5fa05e657e6605e63ed3de7bd534d5ae6d379a5
huangyan <1621740748@qq.com>
2019-11-06 11:04:54 +0800

    修改分词代码

--- src/test/java/com/jrj/test/TrainPrepared.java
+++ src/test/java/com/jrj/test/TrainPrepared.java
@@ -1,10 +1,13 @@
 package com.jrj.test;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.List;
 
 import com.alibaba.fastjson.JSON;
@@ -18,34 +21,60 @@ public class TrainPrepared {
 		String f = "/data/baidu1/news2016zh_train.json";
 		FileInputStream fi = null;
 		BufferedReader fp = null;
+		String fo="/data/baidu1/news2016zh_train.txt";
+		BufferedWriter fw=null;
 		try {
+			fw=new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fo),"utf-8"));
 			fi = new FileInputStream(f);
 			InputStreamReader fileReader = new InputStreamReader(fi, "utf-8");
 			fp = new BufferedReader(fileReader);
-			String line;
+			String line=null;
 			int i = 0;
+			StringBuilder ww=new StringBuilder();
 			do {
-				line = fp.readLine();
-				String l = JSON.parseObject(line).getString("content");
-				List<Term> termList = HanLP.segment(l);
-				for(Term t:termList) {
-					if(CoreStopWordDictionary.contains(t.word)) {
+				try {
+					line = fp.readLine();
+					String l=null;
+					if(line==null) {
+						break;
+					}
+					try {
+						l = JSON.parseObject(line).getString("content");
+					} catch (Exception e) {
+						
+						e.printStackTrace();
+					}
+					if(l==null||l.equals("")) {
 						continue;
 					}
-					System.out.print(t.word+" ");
+					List<Term> termList = HanLP.segment(l);
+					for(Term t:termList) {
+						if(CoreStopWordDictionary.contains(t.word)) {
+							continue;
+						}
+						System.out.print(t.word+" ");
+						ww.append(t.word).append(" ");
+					}
+					//				JiebaSegmenter segmenter = new JiebaSegmenter();
+					//				List<String> aa = segmenter.sentenceProcess(l);
+					//				for (String s : aa) {
+					//					if (CoreStopWordDictionary.contains(s)) {
+					//						continue;
+					//					}
+					//					System.out.print(s+" ");
+					//				}
+					System.out.println();
+					ww.append("\n");
+					i++;
+					if (i>0&&i%10000==0) {
+						String s=ww.toString();
+						fw.write(s);
+						ww.setLength(0);
+					}
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-//				JiebaSegmenter segmenter = new JiebaSegmenter();
-//				List<String> aa = segmenter.sentenceProcess(l);
-//				for (String s : aa) {
-//					if (CoreStopWordDictionary.contains(s)) {
-//						continue;
-//					}
-//					System.out.print(s+" ");
-//				}
-				System.out.println();
-				i++;
-				if (i > 100)
-					break;
 			} while (line != null);
 
 		} catch (FileNotFoundException e) {
@@ -57,6 +86,7 @@ public class TrainPrepared {
 			try {
 				fp.close();
 				fi.close();
+				fw.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
